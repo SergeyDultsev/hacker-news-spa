8eb7611acd7282c1a311fc90e34358149c552583
dultsev <dultsev010904@gmail.com>
2025-08-27 13:13:30 +0700

    feat: adding global style

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import routes from "./router";
+import "@/assets/styles/global.scss";
 
 const app = createApp(App);
 app.use(routes)
--- src/widgets/HeaderApp.vue
+++ src/widgets/HeaderApp.vue
@@ -5,15 +5,54 @@
 <template>
   <header class="header">
     <nav class="navbar">
-      <ul>
-        <li>Best</li>
-        <li>Asks</li>
-        <li>Job</li>
+
+      <!-- Logo -->
+      <div></div>
+
+      <ul class="nav-list">
+        <li class="nav-item">Best</li>
+        <li class="nav-item">Asks</li>
+        <li class="nav-item">Job</li>
       </ul>
+
     </nav>
   </header>
 </template>
 
 <style scoped>
+.header {
+  width: 100%;
+  height: 60px;
+  background-color: var(--orange-color);
+}
+
+.navbar {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px;
+}
+
+.nav-list {
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  gap: 10px;
+}
+
+.nav-item {
+  color: var(--orange-color);
+  background-color: var(--write-color);
+  padding: 8px 10px;
+  border-radius: var(--border-radius-small);
+  transition: all .3s ease-in;
+
+  &:hover {
+    cursor: pointer;
+    color: var(--write-color);
+    background-color: var(--dark-orange-color);
+    transition: all .3s ease-in;
+  }
+}
 
 </style>

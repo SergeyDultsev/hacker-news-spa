36c976e83a59e969b25c269fd4da20978a54fc66
dultsev <dultsev010904@gmail.com>
2025-08-30 18:02:36 +0700

    feat: init pinia

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,8 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import routes from "./router";
+import routes from "@/app/router";
+import { pinia } from "@/app/pinia";
 import "@/assets/styles/global.scss";
 
 const app = createApp(App);
 app.use(routes)
+app.use(pinia);
 app.mount('#app')

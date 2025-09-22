3fa80665df1fa80749e20684bed1aadad199e653
SergeyDultsev <dultsev010904@gmail.com>
2025-09-22 20:22:35 +0700

    feat: delete page info

--- src/app/layouts/MainLayout.vue
+++ src/app/layouts/MainLayout.vue
@@ -1,10 +1,6 @@
 <script setup lang="ts">
-import { usePostStore } from "@/entities/post/posts";
-
-import {FilterBar, HeaderApp} from "@/widgets";
+import { HeaderApp } from "@/widgets";
 import { FooterApp } from "@/widgets";
-
-const postStore = usePostStore();
 
 defineProps<{
   name?: string
@@ -20,12 +16,6 @@ defineProps<{
         <slot />
       </section>
     </main>
-
-    <section class="page-info__bar">
-      <h2 class="page-info__name">{{ name }}</h2>
-      <p class="page-info__item">Posts Count: {{ postStore.countPosts }}</p>
-      <FilterBar />
-    </section>
 
     <FooterApp />
   </section>
@@ -50,29 +40,6 @@ defineProps<{
 .main-content {
   padding: 20px;
   height: 100%;
-}
-
-.page-info__bar {
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-  position: fixed;
-  top: 20px;
-  max-width: 250px;
-  width: 100%;
-  margin: 60px 0 0 20px;
-}
-
-.page-info__name{
-  color: var(--orange-color);
-  font-size: 24px;
-  font-weight: 900;
-}
-
-.page-info__item{
-  color: var(--orange-color);
-  font-size: 16px;
-  font-weight: 600;
 }
 
 @media screen and (max-width: 500px) {

9a68d736c37ca6cf403d6eae94e0ceeea401e3c6
SergeyDultsev <dultsev010904@gmail.com>
2025-11-15 17:38:30 +0700

    feat: update configs

--- frontend/.eslintrc.ts
+++ frontend/.eslintrc.ts
@@ -5,21 +5,27 @@ module.exports = {
         node: true,
         es2024: true,
     },
-    extends: [
-        'plugin:vue/recommended',
-    ],
+    parser: 'vue-eslint-parser',
     parserOptions: {
         ecmaVersion: 'latest',
         parser: '@typescript-eslint/parser',
         sourceType: 'module',
     },
+    extends: [
+        'plugin:vue/vue3-recommended',
+    ],
+    plugins: [
+        '@typescript-eslint'
+    ],
     rules: {
         'vue/multi-word-component-names': 'off',
+
         'no-unused-vars': ['warn', {
             vars: 'all',
             args: 'after-used',
             ignoreRestSiblings: false,
         }],
+
         'comma-dangle': ['error', {
             arrays: 'always-multiline',
             objects: 'always-multiline',
@@ -27,6 +33,7 @@ module.exports = {
             exports: 'always-multiline',
             functions: 'never',
         }],
+
         semi: ['error', 'always'],
         quotes: ['error', 'single'],
     },

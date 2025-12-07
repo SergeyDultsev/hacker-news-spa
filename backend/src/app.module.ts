9f47bfe8901889d01fc460d2af529db6e67d28ce
SergeyDultsev <dultsev010904@gmail.com>
2025-12-08 00:00:12 +0700

    ref: uninstallation Prisma, install to Type ORM

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './resources/user/user.module';
@@ -8,8 +8,15 @@ import { CommentModule } from './resources/comment/comment.module';
 
 @Module({
   imports: [
-      ConfigModule.forRoot({
-        isGlobal: true,
+      TypeOrmModule.forRoot({
+        type: 'postgres',
+        host: process.env.POSTGRES_HOST,
+        port: Number(process.env.POSTGRES_PORT),
+        username: process.env.POSTGRES_USER,
+        password: process.env.POSTGRES_PASSWORD,
+        database: process.env.POSTGRES_DB,
+        autoLoadEntities: true,
+        synchronize: true,
       }),
       UserModule,
       PostModule,
